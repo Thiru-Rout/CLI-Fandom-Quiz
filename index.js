d2a8d91ec01d4d5fd480e16a3ae3ffab1e013327
Thiru <89295206+Thiru-GitHub-code@users.noreply.github.com>
2021-10-08 04:39:25 +0000

    chore:changed the whole content.

--- index.js
+++ index.js
@@ -1,102 +1,128 @@
-var questionOne = {
-    question: "1. In marvel what is Captain America's real name?\n",
-    answer: "Tony Stark"
-  }
-  var questionTwo = {
-    question: "2. What is the tree called that hits people in Harry Potter?\n",
-    answer: "Whomping Willow"
-  }
-  var questionThree = {
-    question: "3. Percy Jackson is the son of?\n",
-    answer: "Poseidon"
-  }
-  var questionFour = {
-    question: "4. Who won the hunger games?\n",
-    answer: "Katniss"
-  }
-  var questionFive = {
-    question: "5. Who is playing the lead role in 'Cyberpunk 2077'?\n",
-    answer: "keanu reeves"
-  }
-  
-  var questionAnswer = [questionOne, questionTwo, questionThree, questionFour, questionFive];
-  
-  function period(){
-    console.log(chalk.blue("----------------------"))
-  }
-  
-  var readlineSync = require("readline-sync");
-  
-  var chalk = require("chalk")
-  
-  var score = 0;
-  
-  var userName = readlineSync.question("What's your name?\n");
-  
-  console.log("Aah!, lovely! Welcome " + userName + ".");
-  period()
-  
-  chooseToPlay();
-  
-  function chooseToPlay(){
-  
-    var wantToPlay = readlineSync.question("Here's a quiz by Thiru, Do you want to play?\n");
-    
-    if (wantToPlay === "yes" || "Yes") {
-      period()
-      console.log(chalk.cyan("Yay! Let's complete the quiz."))
-      period();
-      question();
-    } else if (wantToPlay != "yes" || "Yes") {
-      period()
-      console.log(chalk.yellow("I wish you played this game, sorry to see you go."))
-    }
-  };
-  
-  
-  function playQuiz (question, answer){
-    var userAnswer = readlineSync.question(question);
-    if (userAnswer.toLowerCase() === answer.toLowerCase()){
-      
-      console.log(chalk.green("You got the right answer."))
-      score = score + 1
-    } else {
-      
-      console.log(chalk.red("You messed-up."))
-      console.log(chalk.bgRed("The correct answer is " + answer + "."))
-      score = score - 1
-    }
-    
-    console.log("Your score is " + score + ".")
-    period()
-    period()
-  }
-  
-  function question (){
-    for (var i=0; i<questionAnswer.length; i++){
-      currentTarget = questionAnswer[i];
-      playQuiz(currentTarget.question, currentTarget.answer);
-    }
-  }
-  
-  console.log(chalk.blueBright.bold("Your final score is " + score + "."))
-  period()
-  console.log(chalk.bgYellow("Share this quiz with you friends and also tell me your score."))
-  period()
-  
-  
-  var highScore = [{
-    name: "naveen",
-    score: 5},
-    {
-      name: "nirmal",
-      score: 3
-    },
-  ]
-  
-    console.log(chalk.italic("All the high scores."))
-    period()
-  
-  for (var i = 0; i < highScore.length; i++){
-      console.log(highScore[i].name, ":", highScore[i].score);
-    }
+var readlineSync = require('readline-sync');
+
+var chalk = require('chalk');
+
+var score = 0;
+
+var highScores = [
+	{
+		name: "Dinesh",
+		score: 6,
+	}
+]
+
+
+var questions = [
+	{
+		question: "1. In cricket, who is an all-rounder?",
+		options: [
+			"a. Who can field in all positions",
+			"b. Who can bowl with both hands",
+			"c. Who can bat in all positions",
+			"d. Who can bat and bowl"
+		],
+		answer: "d. Who can bat and bowl",
+	},
+	{
+		question: "2. Which two cricketers were accused in the 'Monkeygate' scandal? ",
+		options: [
+			"a. Sachin Tendulkar & Ricky Ponting",
+			"b. MS Dhoni & Andrew Symonds",
+			"c. Harbhajan Singh & Andrew Symonds",
+			"d. Zaheer Khan & Adam Gilchrist"
+		],
+		answer: "c. Harbhajan Singh & Andrew Symonds",
+	},
+	{
+		question: "3. What is the only way that a batsman can get out in a 'free hit'? ",
+		options: [
+			"a. Caught behind",
+			"b. Run out",
+			"c. Stumping",
+			"d. Bowled"
+		],
+		answer: "b. Run out",
+	},
+	{
+		question: "4. Where was the opening ceremony of the 2011 ICC Cricket World Cup held? ",
+		options: [
+			"a. Kolkata",
+			"b. Dhaka",
+			"c. Colombo",
+			"d. Bengaluru"
+		],
+		answer: "b. Dhaka",
+	},
+	{
+		question: "5. Apart from India, for which other country did Nawab of Pataudi senior play cricket? ",
+		options: [
+			"a. South Africa",
+			"b. New Zealand",
+			"c. England",
+			"d. Australia"
+		],
+		answer: "c. England",
+	},
+]
+
+
+function welcome() {
+	var user = readlineSync.question("What's your name? ");
+
+	console.log(`\nWelcome to Cricket quiz ${user}.`);
+}
+
+
+function play(question,options, answer) {
+	console.log(chalk.blue(question));
+	
+	console.log(chalk.yellow("\nOptions:\n"))
+	for(let i =0; i< options.length; i++){
+		console.log(options[i]);
+	}
+
+	var userAns = readlineSync.question("\nEnter your answer: ");
+	console.log(`you entered ${userAns}`);
+
+	if (userAns.toUpperCase() == answer.toUpperCase()) {
+		console.log(chalk.green("You're right"));
+		score = score + 2;
+	} else {
+		console.log(chalk.red("You're wrong!"));
+	}
+	console.log(`SCORE: ${score}`);
+}
+
+
+
+function game() {
+	for (let i = 0; i < questions.length; i++) {
+		let currentQuestion = questions[i];
+		console.log(`\nQuestion ${i + 1}:\n-----------`)
+		play(currentQuestion.question,currentQuestion.options, currentQuestion.answer)
+	}
+}
+
+
+
+function checkHighScore() {
+	if (score > 0) {
+		console.log(`\n\nWOW!! You scored ${score}`);
+	} else if (score === 0) {
+		console.log(`\n\nYou scored ${score}. Its ok that you don't know me.`);
+	}
+	else {
+		console.log(`\n\nYou scored ${score}`);
+	}
+	for (let i = 0; i < highScores.length; i++) {
+		let currentScore = highScores[i];
+		if (score > currentScore.score) {
+			console.log(chalk.green(`Congratulations!! You made a highscore. You beat ${currentScore.name} by ${score - currentScore.score} `))
+			console.log('You can send me a screenshot to get your score to be added to the highscore list.')
+		}
+	}
+}
+
+welcome();
+game();
+checkHighScore();
